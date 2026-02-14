0fa9847ac215c85d856266fe61e9d596cd0483c9
katemaslova <maslova.ekaterina@phystech.edu>
2026-02-14 19:20:56 +0300

    Rewrited with functions

--- cat.asm
+++ cat.asm
@@ -3,106 +3,194 @@
 org 100h
 .286
 
-Start:      mov ax, 0b800h              ; загружаем в es адрес начала видеопамяти
-            mov es, ax                  ;
+Start:
 
-            xor ax, ax                  ; зануляем ax
-            mov al, len                 ; кладём длину командной строки в al
-            dec al                      ; уменьшаем длину(не считаем первый пробел)
-            shr ax, 1                   ;
-            shl ax, 1                   ; если ax нечётно, то делаем чётным
-            mov di, 160d * 10 + 80d     ; сдвиг на середину экрана
-            sub di, ax ;                ; отнимаем длину слова(размещаем слово посередине)
-
-            mov cl, len                 ; кладём длину командной строки в cl
-            dec cl                      ; уменьшаем длину(не считаем первый пробел)
-            mov ah, color               ; кладём атрибут символа в ah
-            mov si, cmd_start           ; кладём адрес начала командной строки в si
-
-Phrase:                                 ; печать фразы
-            lodsb
-            stosw
-LOOP Phrase
-
-            mov di, 160d * 8 + 40d          ; кладём в di адрес левого верхнего угла
-            mov cx, 34d                     ; кладём в cx длину рамки
+COLOR       equ 0bdh            ; цвет
+LEN         equ ds:[80h]        ; длина командной строки
+CMD_START   equ 082h            ; адрес начала командной строки
+SYMBOL      equ 03h             ; символ
 
 
-FrameUpperRow:
-            mov al, symbol
-            mov byte ptr es:[di+06h], al    ; загружаем нужный символ в es:[di]
-            mov byte ptr es:[di+07h], color ; загружаем атрибут символа в соседний байт
-            inc di                          ; дважды инкрементируем di
-            inc di                          ;
-LOOP FrameUpperRow
+            call SetVideoMemoryStart
+            call SetPhrasePlace
+            call SetPhraseLength
+            call SetSymbol
+            call SetCmdStartParam
 
-            mov di, 160d * 5 + 40d          ; кладём в di адрес левого верхнего угла
-            mov cx, 7h                      ; кладём в сх ширину рамки
+            call PrintPhrase
 
+            call SetUpperRowParams
+            call PrintUpperRow
 
-FrameLeftColumn:
-            mov al, symbol
-            mov byte ptr es:[di], al        ; загружаем нужный символ в es:[di]
-            mov byte ptr es:[di+01h], color ; загружаем атрибут символа в соседний байт
-            add di, 160d                    ; увеличиваем di на 160d (переход на следующую строку)
-LOOP FrameLeftColumn
+            call SetLeftColumnParams
+            call PrintLeftColumn
 
-            mov di, 160d * 5 + 120d - 2     ; кладём в di адрес правого верхнего угла
-            mov cx, 7h                      ; кладём в сх ширину рамки
+            call SetRightColumnParams
+            call PrintRightColumn
 
+            call SetLowerRowParams
+            call PrintLowerRow
 
-FrameRightColumn:
-            mov al, symbol
-            mov byte ptr es:[di], al        ; загружаем нужный символ в es:[di]
-            mov byte ptr es:[di+01h], color ; загружаем атрибут символа в соседний байт
-            add di, 160d                    ; увеличиваем di на 160d (переход на следующую строку)
-LOOP FrameRightColumn
+            call PrintLeftEar
+            call PrintRightEar
 
-            mov di, 160d * 12 + 40d         ; кладём в di адрес левого нижнего угла
-            mov cx, 40d                     ; кладём в cx длину рамки
+            call Exit
 
 
-FrameLowerRow:
-            mov al, symbol
-            mov byte ptr es:[di], al        ; загружаем нужный символ в es:[di]
-            mov byte ptr es:[di+01h], color ; загружаем атрибут символа в соседний байт
-            inc di                          ; дважды инкрементируем di
-            inc di                          ;
-LOOP FrameLowerRow
+SetVideoMemoryStart     proc
 
-            mov di, 160d * 6 + 40d + 2d     ; дорисовать левый край
-            mov al, symbol
-            mov byte ptr es:[di], al
-            mov byte ptr es:[di+01h], color ; stosw - переделать
+                        mov ax, 0b800h              ; загружаем в es адрес начала видеопамяти
+                        mov es, ax                  ;
 
-            mov di, 160d * 7 + 40d + 4d
-            mov al, symbol
-            mov byte ptr es:[di], al
-            mov byte ptr es:[di+01h], color
+                        ret
 
-            mov di, 160d * 6 + 120d - 4d     ; дорисовать правый край
-            mov al, symbol
-            mov byte ptr es:[di], al
-            mov byte ptr es:[di+01h], color
+SetPhrasePlace          proc
 
-            mov di, 160d * 7 + 120d - 6d
-            mov al, symbol
-            mov byte ptr es:[di], al
-            mov byte ptr es:[di+01h], color
+                        xor ax, ax                  ; зануляем ax
+                        mov al, LEN                 ; кладём длину командной строки в al
+                        dec al                      ; уменьшаем длину(не считаем первый пробел)
+                        shr ax, 1                   ;
+                        shl ax, 1                   ; если ax нечётно, то делаем чётным
+                        mov di, 160d * 10 + 80d     ; сдвиг на середину экрана
+                        sub di, ax                  ; отнимаем длину слова(размещаем слово посередине)
 
-PrintRow:
-        ;
+                        ret
 
+SetSymbol               proc
 
-        ;
+                        mov ah, COLOR               ; кладём атрибут символа в ah
 
-            mov ax, 4c00h       ; завершение программы
-            int 21h             ;
+                        ret
 
-color       equ 0bdh            ; цвет
-len         equ ds:[80h]        ; длина командной строки
-cmd_start   equ 082h            ; адрес начала командной строки
-symbol      equ 03h             ; символ
+SetPhraseLength         proc
 
+                        mov cl, LEN                 ; кладём длину командной строки в cl
+                        dec cl                      ; уменьшаем длину(не считаем первый пробел)
+
+                        ret
+
+SetCmdStartParam        proc
+
+                        mov si, CMD_START           ; кладём адрес начала командной строки в si
+
+                        ret
+
+PrintPhrase             proc
+
+                        Phrase:                     ; печать фразы
+                            lodsb
+                            stosw
+                        loop Phrase
+
+                        ret
+
+SetUpperRowParams       proc
+
+                        mov di, 160d * 8 + 40d          ; кладём в di адрес левого верхнего угла
+                        mov cx, 34d                     ; кладём в cx длину рамки
+
+                        ret
+
+SetLeftColumnParams     proc
+
+                        mov di, 160d * 5 + 40d          ; кладём в di адрес левого верхнего угла
+                        mov cx, 7h                      ; кладём в сх ширину рамки
+
+                        ret
+
+SetRightColumnParams    proc
+
+                        mov di, 160d * 5 + 120d - 2     ; кладём в di адрес правого верхнего угла
+                        mov cx, 7h                      ; кладём в сх ширину рамки
+
+                        ret
+
+SetLowerRowParams       proc
+
+                        mov di, 160d * 12 + 40d         ; кладём в di адрес левого нижнего угла
+                        mov cx, 40d                     ; кладём в cx длину рамки
+
+                        ret
+
+PrintUpperRow           proc
+
+                        UpperRow:
+                            mov al, SYMBOL
+                            mov byte ptr es:[di+06h], al    ; загружаем нужный символ в es:[di]
+                            mov byte ptr es:[di+07h], COLOR ; загружаем атрибут символа в соседний байт
+                            inc di                          ; дважды инкрементируем di
+                            inc di                          ;
+                        loop UpperRow
+
+                        ret
+
+PrintLeftColumn         proc
+
+                        LeftColumn:
+                            mov al, SYMBOL
+                            mov byte ptr es:[di], al        ; загружаем нужный символ в es:[di]
+                            mov byte ptr es:[di+01h], COLOR ; загружаем атрибут символа в соседний байт
+                            add di, 160d                    ; увеличиваем di на 160d (переход на следующую строку)
+                        loop LeftColumn
+
+                        ret
+
+PrintRightColumn        proc
+
+                        RightColumn:
+                            mov al, SYMBOL
+                            mov byte ptr es:[di], al        ; загружаем нужный символ в es:[di]
+                            mov byte ptr es:[di+01h], COLOR ; загружаем атрибут символа в соседний байт
+                            add di, 160d                    ; увеличиваем di на 160d (переход на следующую строку)
+                        loop RightColumn
+
+                        ret
+
+PrintLowerRow           proc
+
+                        LowerRow:
+                            mov al, SYMBOL
+                            mov byte ptr es:[di], al        ; загружаем нужный символ в es:[di]
+                            mov byte ptr es:[di+01h], COLOR ; загружаем атрибут символа в соседний байт
+                            inc di                          ; дважды инкрементируем di
+                            inc di                          ;
+                        loop LowerRow
+
+                        ret
+
+PrintLeftEar            proc
+
+                        mov di, 160d * 6 + 40d + 2d     ; дорисовать левый край
+                        mov al, SYMBOL
+                        mov byte ptr es:[di], al
+                        mov byte ptr es:[di+01h], COLOR ; stosw - переделать
+
+                        mov di, 160d * 7 + 40d + 4d
+                        mov al, SYMBOL
+                        mov byte ptr es:[di], al
+                        mov byte ptr es:[di+01h], COLOR
+
+                        ret
+
+PrintRightEar           proc
+
+                        mov di, 160d * 6 + 120d - 4d     ; дорисовать правый край
+                        mov al, SYMBOL
+                        mov byte ptr es:[di], al
+                        mov byte ptr es:[di+01h], COLOR
+
+                        mov di, 160d * 7 + 120d - 6d
+                        mov al, SYMBOL
+                        mov byte ptr es:[di], al
+                        mov byte ptr es:[di+01h], COLOR
+
+                        ret
+
+Exit                    proc
+
+                        mov ax, 4c00h       ; завершение программы
+                        int 21h             ;
+
+                        ret
 
 end         Start
